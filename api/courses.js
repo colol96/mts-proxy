ec9fb7e9c1cb4b9c643f29786c5fb7f4bdb4f6fd
odnorad <nicoleang96@gmail.com>
2025-08-24 20:09:41 +0200

    updated courses.js -> wrapped title in section

--- api/courses.js
+++ api/courses.js
@@ -61,7 +61,7 @@ function renderSectionsHTML(grouped) {
     const sections = grouped.map(g => sectionHTML(g.title, g.items)).join('');
     return `
     <div class="mts-wrapper">
-      <h2 class="mts-main-title">More courses to explore</h2>
+      <section class="mts-section"><h2 class="mts-main-title">More courses to explore</h2></section>
       ${sections}
     </div>
   `;

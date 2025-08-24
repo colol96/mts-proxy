621e32066497a638eeca0ea0c7f7a4e6bda30eca
odnorad <nicoleang96@gmail.com>
2025-08-24 19:47:53 +0200

    updated courses.js -> removed comma after teacher

--- api/courses.js
+++ api/courses.js
@@ -42,7 +42,7 @@ function teacherSpan(t) {
 }
 
 function courseCardHTML(c) {
-    const teachersHTML = (c.teachers || []).map(teacherSpan);
+    const teachersHTML = (c.teachers || []).map(teacherSpan).join('');
     return `
     <a class="mts-card" href="https://www.masterthescore.com/courses/${c.slug}">
       ${c.image ? `<img src="${c.image}" alt="">` : ''}
